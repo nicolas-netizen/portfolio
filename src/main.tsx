5e9bd701cf2b921749eb054bd5b378d8a41e87d8
nicolas paniagua <nico2005coki@gmail.com>
2024-11-21 14:24:33 -0300

    feat: Add Vercel Analytics integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import './i18n';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import './i18n'
+import { HelmetProvider } from 'react-helmet-async';
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <HelmetProvider>
+      <App />
+      <Analytics />
+    </HelmetProvider>
+  </React.StrictMode>,
+)
